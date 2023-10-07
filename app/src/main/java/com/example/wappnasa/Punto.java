1c271f4f2a1d25fab127a8072ea38e432325d2e5
zarlorcode <lorena.zarapo@gmail.com>
2023-10-07 12:28:02 +0200

    Getters y setters

--- app/src/main/java/com/example/wappnasa/Punto.java
+++ app/src/main/java/com/example/wappnasa/Punto.java
@@ -11,4 +11,43 @@ public class Punto {
     double longitud;
     @DatabaseField
     int zona;
+    public Punto(){}
+    public Punto(int idPunto, double latitud, double longitud, int zona) {
+        this.idPunto = idPunto;
+        this.latitud = latitud;
+        this.longitud = longitud;
+        this.zona = zona;
+    }
+
+    public int getIdPunto() {
+        return idPunto;
+    }
+
+    public void setIdPunto(int idPunto) {
+        this.idPunto = idPunto;
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(double latitud) {
+        this.latitud = latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+
+    public int getZona() {
+        return zona;
+    }
+
+    public void setZona(int zona) {
+        this.zona = zona;
+    }
 }
--- app/src/main/java/com/example/wappnasa/Zona.java
+++ app/src/main/java/com/example/wappnasa/Zona.java
@@ -7,4 +7,26 @@ public class Zona {
     int idZona;
     @DatabaseField
     String descripcion;
+    public Zona(){}
+
+    public Zona(int idZona, String descripcion) {
+        this.idZona = idZona;
+        this.descripcion = descripcion;
+    }
+
+    public int getIdZona() {
+        return idZona;
+    }
+
+    public void setIdZona(int idZona) {
+        this.idZona = idZona;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
